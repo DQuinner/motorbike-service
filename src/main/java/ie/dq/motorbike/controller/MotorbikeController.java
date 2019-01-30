c443bff8eb7ca40611d17f4691cf1859783c73fd
Donal Quinn <donal.quinn@wearebionic.com>
2019-01-30 11:37:40 +0000

    added logger messages

--- src/main/java/ie/dq/motorbike/controller/MotorbikeController.java
+++ src/main/java/ie/dq/motorbike/controller/MotorbikeController.java
@@ -2,6 +2,8 @@ package ie.dq.motorbike.controller;
 
 import ie.dq.motorbike.domain.Motorbike;
 import ie.dq.motorbike.service.MotorbikeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MotorbikeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MotorbikeController.class);
+
     private static final long ZERO = 0l;
 
     @Autowired
@@ -21,20 +25,26 @@ public class MotorbikeController {
 
     @GetMapping("/motorbikes")
     public ResponseEntity<Page<Motorbike>> getMotorbikes(){
+        logger.info("getMotorbikes()");
         Page<Motorbike> motorbikes = motorbikeService.getMotorbikes();
         if(motorbikes.hasContent()){
+            logger.info("Found {} motorbikes", motorbikes.getTotalElements());
             return new ResponseEntity(motorbikes, HttpStatus.OK);
         }else{
+            logger.info("No motorbikes found");
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         }
     }
 
     @PostMapping("/motorbikes")
     public ResponseEntity<Motorbike> createMotorbike(@RequestBody Motorbike motorbike){
+        logger.info("createMotorbike {} {}",motorbike.getMake(), motorbike.getModel());
         Motorbike result = motorbikeService.createMotorbike(motorbike);
         if(result.getId()>ZERO){
+            logger.info("Motorbike {} {} created with id {}",result.getMake(), result.getModel(), result.getId());
             return new ResponseEntity(result, HttpStatus.CREATED);
         }else{
+            logger.error("Motorbike {} {} already exists", motorbike.getMake(), motorbike.getModel());
             return new ResponseEntity(motorbike, HttpStatus.CONFLICT);
         }
     }
--- src/main/java/ie/dq/motorbike/service/impl/MotorbikeServiceImpl.java
+++ src/main/java/ie/dq/motorbike/service/impl/MotorbikeServiceImpl.java
@@ -3,6 +3,8 @@ package ie.dq.motorbike.service.impl;
 import ie.dq.motorbike.domain.Motorbike;
 import ie.dq.motorbike.repository.MotorbikeRepository;
 import ie.dq.motorbike.service.MotorbikeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,20 +14,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class MotorbikeServiceImpl implements MotorbikeService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MotorbikeServiceImpl.class);
+
     @Autowired
     private MotorbikeRepository motorbikeRepository;
 
     @Override
     public Motorbike createMotorbike(Motorbike motorbike) {
+        logger.info("createMotorbike {} {}", motorbike.getMake(), motorbike.getModel());
         if(motorbikeRepository.existsMotorbikeByMakeAndModel(motorbike.getMake(), motorbike.getModel())){
+            logger.info("Motorbike {} {} already exists", motorbike.getMake(), motorbike.getModel());
             return new Motorbike();
         }else{
+            logger.info("Saving motorbike {} {}", motorbike.getMake(), motorbike.getModel());
             return motorbikeRepository.save(motorbike);
         }
     }
 
     @Override
     public Page<Motorbike> getMotorbikes() {
+        logger.info("getMotorbikes()");
         return motorbikeRepository.findAll(PageRequest.of(0, 1000, Sort.by(Sort.Direction.ASC, "make")));
     }
 }
